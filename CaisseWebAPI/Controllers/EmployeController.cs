305ea5107e2dee679f43c04e087cb33d53226de4
Alexandre Bouffard <bouffarda7@outlook.com>
2019-10-02 16:55:27 -0400

    Structure initial

---
+++ CaisseWebAPI/Controllers/EmployeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CaisseWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EmployeController> _logger;
+
+        public EmployeController(ILogger<EmployeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public string ConnexionEmploye(object EmployeCred)
+        {
+            return "";
+        }
+    }
+}
