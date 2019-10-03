06c31002b74e683aa2d4fecbfda4b698715726b1
Alexandre Bouffard <bouffarda7@outlook.com>
2019-10-03 09:16:47 -0400

    Ajout d'un utilitaire pour la gestion des mots de passe

---
+++ CaisseWebDAL/Helpers/PasswordHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaisseWebDAL.Helpers
+{
+    public static class PasswordHelper
+    {
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+
+        public static string HashPassword(string passwordToHash)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(passwordToHash);
+        }
+    }
+}
